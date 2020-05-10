efbea6a47dd70c4fa3e6bafc5a5f7ff71fea31a2
Stephanie Sohns <ssohns@gmail.com>
2020-05-10 14:45:25 -0600

    Add book test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+
+function createTitle(title) {
+    return `The ${title}`
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, reviews) {
+  if(!reviews.includes(review)) {
+    reviews.push(review);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var bookObj = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: "fantasy",
+  }
+  return bookObj;
+}
+
+function editBook(ghoulBook) {
+  ghoulBook.pageCount = ghoulBook.pageCount * .75;
+  return ghoulBook.pageCount;
+}
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
